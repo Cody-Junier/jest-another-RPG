c511382558da55b347ad69c01586fff736238ee1
cody-junier <codyjunier@gmail.com>
2021-12-08 21:28:23 -0800

    Finished enemy object

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -65,7 +65,7 @@ test("subtracts from player's health", () => {
 
 test("gets player's attack value", () =>{
     const player = new Player('Dave')
-    player.strenght = 10;
+    player.strength = 10;
     expect(player.getAttackValue()).toBeGreaterThanOrEqual(5);
     expect(player.getAttackValue()).toBeLessThanOrEqual(15);
 });
